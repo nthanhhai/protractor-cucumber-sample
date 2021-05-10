1f70f1e8bcc3249465fc9094e6c05ed74b50c8a7
Nguyen, Hai <hai.nguyen@news.com.au>
2021-05-10 22:25:37 +1000

    first commit

---
+++ conf/conf.local.js
@@ -0,0 +1,43 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  getPageTimeout: 60000,
+  allScriptsTimeout: 600000,
+  framework: 'custom',
+  // path relative to the current config file
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  capabilities: {
+    browserName: 'chrome',
+    acceptSslCerts: 'true',
+  },
+  SELENIUM_PROMISE_MANAGER: false,
+  // Spec patterns are relative to this directory.
+  specs: [
+    '../feature_files/*.feature'
+  ],
+
+  params: {
+    baseUrl: '',
+    downloadedPdf: '',
+  },
+
+  onPrepare: function () {
+    //browser.ignoreSynchronization = true;`
+    browser.waitForAngularEnabled(false);
+    if (!process.env.SITE_URL) {
+      browser.params.baseUrl = 'https://sit.newsconnect.com.au';
+      // browser.params.baseUrl = 'http://localhost:3000';
+    } else {  
+      browser.params.baseUrl = process.env.SITE_URL;
+    }
+  },
+
+  cucumberOpts: {
+    require: ['../step_definitions/*.js'],
+    tags: ['@TEST'],
+    format: 'json:reports/TestResults.json',
+    profile: false,
+    'no-source': true
+  },
+
+  ignoreUncaughtExceptions: true
+};
